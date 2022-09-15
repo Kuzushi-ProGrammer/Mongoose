6607428f97be920495ec133371c1a74e06eea157
Dominique Spavor (Kuzushi) <derp.rosy@gmail.com>
2022-09-15 11:47:53 -0600

    Testing out classes and variables (not important)

---
+++ Assets/Scripts/TestVar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVar : MonoBehaviour
+{
+
+    public static string splatoon = "squid";
+
+    // public makes the variable visible
+    // static makes the variable the same across all instances of the object with the script on it
+
+}
---
+++ Assets/Scripts/TestVar2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVar2 : MonoBehaviour
+{
+
+    string splat2oon = TestVar.splatoon;
+
+    private void Start()
+    {
+        print(splat2oon);
+    }
+}
