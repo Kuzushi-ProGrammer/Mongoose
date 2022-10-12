55e8f96ec1668f6162fb8c207cffc48809babae7
softyydev <vanaalstgrady@gmail.com>
2022-10-12 09:33:07 -0600

    menu shenaniganry

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public int SceneManager;
+
+    public GameObject MainMenu;
+    public GameObject LevelMenu;
+    public GameObject MultiplayerMenu;
+    public GameObject OptionsMenu;
+
+    private GameObject sButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // sButton = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
