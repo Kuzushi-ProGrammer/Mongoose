135c0bbb9b7c2eecc6b10a2f438fef495e8a6fac
ED-31 <113193409+ED-31@users.noreply.github.com>
2022-11-21 09:42:02 -0700

    enemy moves in a unpredictable manor
    it do work now kinda

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -7,16 +8,20 @@ public class EnemyController : MonoBehaviour
     public float health = 3;
     public bool hasBeenShot = false;
     GameObject newbullet;
-    
+
     public GameObject bulletPrefab;
-    public GameObject bulletSpawnPoint; 
-    int bulletOnSpeed ;
+    public GameObject bulletSpawnPoint;
+    int bulletSpeed = 100;
     bool hasShotBack1 = false;
-    
+    DIRECTION enemyDirection = DIRECTION.DOWN;
+    Rigidbody2D enemyRB;
+    int enemySpeed = 10;
+    bool changeDirectionCoolDown = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,13 +29,17 @@ public class EnemyController : MonoBehaviour
     {
         if (hasBeenShot == true)
         {
-                gunGoBoom();
+            gunGoBoom();
         }
+        handleMovement();
+        changeDirection();
+       
+
     }
     void gunGoBoom()
     {
         hasBeenShot = false;
-        newbullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform);
+        //newbullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, transform.rotation);
         Debug.Log("bang bang bang");
     }
 
@@ -40,7 +49,7 @@ public class EnemyController : MonoBehaviour
         {
             health--;
             hasBeenShot = true;
-            Debug.Log("you've shot me what did I so to you?");
+            Debug.Log("you've shot me what did I do to you?");
             if (health == 0)
             {
                 Destroy(gameObject);
@@ -48,4 +57,52 @@ public class EnemyController : MonoBehaviour
             }
         }
     }
+    void changeDirection()
+    {
+        
+        StartCoroutine(changeDirectionCoolDownTimer());
+        if (changeDirectionCoolDown == false)
+        {
+            if (numbers() <= 6)
+            {
+                enemyDirection = (DIRECTION)Random.Range(0, 5);
+                changeDirectionCoolDown = true;
+            }
+        }
+    }
+        int numbers()
+        {
+            int randomNumber = Random.Range(1, 5000);
+            return randomNumber;
+        }
+        void handleMovement()
+        {
+            switch (enemyDirection)
+            {
+                case DIRECTION.DOWN:
+                    enemyRB.velocity = new Vector2(0f, -enemySpeed);
+                    break;
+                case DIRECTION.UP:
+                    enemyRB.velocity = new Vector2(0f, enemySpeed);
+                    break;
+                case DIRECTION.RIGHT:
+                    enemyRB.velocity = new Vector2(enemySpeed, 0f);
+                    break;
+                case DIRECTION.LEFT:
+                    enemyRB.velocity = new Vector2(-enemySpeed, 0f);
+                    break;
+                case DIRECTION.STAY:
+                    enemyRB.velocity = new Vector2(0f, 0f);
+                    break;
+
+            }
+        }
+        IEnumerator changeDirectionCoolDownTimer()
+        {
+            yield return new WaitForSeconds(2);
+            changeDirectionCoolDown = false;
+            Debug.Log("I could turn but I don't feel like it");
+        }
+    
 }
+
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,11 @@ public class GameManager : MonoBehaviour
     GameObject newPlayer;
     public GameObject playerSpawnPoint;
     int allotedMongeese = 1;
-
+    public GameObject enemyPrefab;
+    GameObject newEnemy;
+    public GameObject enemySpawnPoint1;
+    public GameObject enemySpawnPoint2;
+    public GameObject enemySpawnPoint3;
     private void Awake()
     {
         DontDestroyOnLoad(this);
@@ -34,4 +38,13 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+   
+}
+enum DIRECTION 
+{
+    UP = 0,
+    RIGHT = 1,
+    LEFT = 2,
+    DOWN = 3,
+    STAY = 4,
 }
